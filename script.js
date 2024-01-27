3ec3039f5feb73e5e8b8f05c48152e4579fbbba1
Rahulkumar8409 <122075648+Rahulkumar8409@users.noreply.github.com>
2024-01-28 02:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function firstNonRepeatedChar(str) {
-   if (!str) return null;
-
+   
 	    for (let char of str) {
         if (str.indexOf(char) === str.lastIndexOf(char)) {
             return char;
